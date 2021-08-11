1f767ac9cabdab1568bdcb8b783cb9ab5257759c
kitions <nsplknsplk@qq.com>
2021-08-11 15:49:06 +0800

    <feat>到用户选择的一步了

--- bin/cli.js
+++ bin/cli.js
@@ -1,5 +1,7 @@
 #! /usr/bin/env node
 const program = require('commander')
+const figlet = require('figlet')
+const chalk = require('chalk')
 
 program
     // 定义命令和参数
@@ -9,14 +11,47 @@ program
     .option('-f, --force', 'overwrite target directory if it exist')
     .action((name, options) => {
         // 打印执行结果
-        console.log('name:', name, 'options:', options)
+        // console.log('name:', name, 'options:', options)
+        require("../lib/create.js")(name, options)
     })
 program
     // 配置版本号信息
     .version(`v${require('../package.json').version}`)
     .usage('<command> [option]')
 
+program
+    .command('config [value]')
+    .description('inspect and modify the config')
+    .option('-g, --get <path>', 'get value from option')
+    .option('-s, --set <path> <value>')
+    .option('-d, --delete <path>', 'delete option from config')
+    .action((value, options) => {
+        console.log(value, options)
+    })
+
+program
+    .command('ui')
+    .description('start add open')
+    .option('-p, --port <port>', 'Port used for the UI Server')
+    .action((option) => {
+        console.log(option)
+    })
+
+program
+    .on('--help', () => {
+        // 使用 figlet 绘制 Logo
+        console.log('\r\n' + figlet.textSync('links-cli', {
+            font: 'Ghost',
+            horizontalLayout: 'default',
+            verticalLayout: 'default',
+            width: 120,
+            whitespaceBreak: true
+        }));
+        // 新增说明信息
+        console.log(`\r\nRun ${chalk.cyan(`<command> --help`)} show details\r\n`)
+    })
+
+
+
 // 解析用户执行命令传入参数
 program.parse(process.argv);
-
-console.log('links-cli working ~')
